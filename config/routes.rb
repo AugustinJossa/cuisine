6ff59917c392b6fe0e6806cabbbe1e011dcab25b
Augustin Jossa <augustin.jossa@hec.edu>
2017-12-31 19:41:12 +0100

    creation des models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :recipe_ingredients
+  resources :ingredients
+  resources :recipes
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
